beae4919249d09f883d3452f26f15969bd470234
Zamara-Osorio <zamaraosorio88@gmail.com>
2025-03-27 15:21:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const addNote = (title, text) => {
+    console.log(title,text)
+  }
+
+  return (
+    <>
+    <h1 id="DoList">Do List</h1>
+      <div id="note-info">
+        <input type="text" id="note-title" placeholder='Ingrese el titulo de la nota'></input>
+        <textarea id="note-text" placeholder='Ingrese el texto de la nota'></textarea>
+        <button onClick={() => addNote(document.getElementById("note-title").value,document.getElementById("note-text").value)} id="btnAdd">Add</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
