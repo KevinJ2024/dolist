529ac78a245a7ab2f627a35a3c26e2b30b586143
Zamara-Osorio <zamaraosorio88@gmail.com>
2025-04-03 15:56:56 -0500

    solucion de errores

--- src/components/NoteCard/NoteCard.jsx
+++ src/components/NoteCard/NoteCard.jsx
@@ -14,8 +14,12 @@ export const NoteCard = ({ id, title, text, bgColor, isCompleted }) => {
     <div className="note-card" style={{ backgroundColor: bgColor }}>
       <h1>{title}</h1>
       <div className="note-card-content">
-        <h3>{text}</h3>
-        <input type="checkbox" checked={isCompleted} onChange={handleChange} />
+        <div className="note-text">
+          <h3>{text}</h3>
+        </div>
+        <div className="check-container">
+          <input type="checkbox" checked={isCompleted} onChange={handleChange} />
+        </div>
       </div>
     </div>
   )
